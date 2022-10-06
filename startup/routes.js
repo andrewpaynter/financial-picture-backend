f13d9d1e3584b11561d9588e25ee0af0d8d88a92
Andrew Paynter <andrewpaynter97@gmail.com>
2022-10-06 15:09:47 -0400

    removed helmet

--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const helmet = require('helmet')
 const addHours = require('date-fns/addHours')
 const users = require('../routes/users')
 const auth = require('../routes/auth')
@@ -10,7 +9,6 @@ const cookieParser = require('cookie-parser')
 
 module.exports = function (app) {
   app.use(express.json())
-  app.use(helmet())
   app.use(
     cors({
       origin: [
